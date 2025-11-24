d57366f58160345831d42625e25f3c8f9842bd78
Anton Ignatev <anton.spba@yandex.ru>
2025-11-24 13:48:13 +0300

    i'm bad at tests, but i try hard this time

--- main_test.go
+++ main_test.go
@@ -24,33 +24,20 @@ func TestGenerateRandomElements(t *testing.T) {
 }
 
 func TestMaximum(t *testing.T) {
-	testCases := [][]int{
-		{},
-		{42},
-		{5},
-		{1, 2, 3},
-		{10, 5, 8, 3, 9, 1},
-		{10, 5, 1, 20},
-		{0, 0, 0},
-		{100, 200, 300, 250, 150},
+	testCases := []struct {
+		input    []int
+		expected int
+	}{
+		{[]int{}, 0},
+		{[]int{42}, 42},
+		{[]int{10, 5, 20}, 20},
+		{[]int{3, 1, 4, 1, 5, 9, 2}, 9},
+		{[]int{0, 0, 0}, 0},
+		{[]int{7, 3, 7, 2}, 7},
 	}
 
-	for _, data := range testCases {
-		expected := 0
-		if len(data) == 0 {
-			expected = 0
-		} else if len(data) == 1 {
-			expected = data[0]
-		} else {
-			expected = data[0]
-			for _, v := range data {
-				if v > expected {
-					expected = v
-				}
-			}
-		}
-
-		actual := maximum(data)
-		assert.Equal(t, expected, actual, "Для данных %v максимум должен быть %d", data, expected)
+	for _, tc := range testCases {
+		result := maximum(tc.input)
+		assert.Equal(t, tc.expected, result)
 	}
 }
